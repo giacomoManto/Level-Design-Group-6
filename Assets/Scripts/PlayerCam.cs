8dc94b1c23c1c519cea86c3de4c9071d4a86eaf3
Enator18 <62544405+Enator18@users.noreply.github.com>
2025-02-05 21:03:03 -0500

    add player

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensitivityX;
+    public float sensitivityY;
+
+    public Transform orientation;
+
+    private float rotationX;
+    private float rotationY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensitivityX * Time.deltaTime;
+        float mouseY= Input.GetAxisRaw("Mouse Y") * sensitivityY * Time.deltaTime;
+
+        rotationY += mouseX;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+        orientation.rotation = Quaternion.Euler(0, rotationY, 0);
+
+
+    }
+}
