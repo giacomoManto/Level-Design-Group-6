e48a1e475effa65a7d292a1ac34059a3edc4d857
Melon-3 <165518419+Melon-3@users.noreply.github.com>
2025-03-12 23:26:11 -0400

    Enter Pod Functionality (Part 1)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,9 +20,11 @@ public class PlayerMovement : MonoBehaviour
 
     private float vertRotation;
     private float ogMoveSpeed;
+    public bool isPlaying;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isPlaying = true;
         Cursor.lockState = CursorLockMode.Locked;
         originalScale = this.transform.localScale.y;
         ogMoveSpeed = moveSpeed;
@@ -31,65 +33,73 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool onGround = controller.isGrounded;
-        float mouseX = Input.GetAxisRaw("Mouse X");
-        float mouseY = Input.GetAxisRaw("Mouse Y");
+        if(isPlaying)
+        {
+            bool onGround = controller.isGrounded;
+            float mouseX = Input.GetAxisRaw("Mouse X");
+            float mouseY = Input.GetAxisRaw("Mouse Y");
 
-        if (onGround)
-        {
-            yVelocity = 0;
-        }
-        else
-        {
+            if (onGround)
+            {
+                yVelocity = 0;
+            }
+            else
+            {
+                yVelocity -= 0.5f * gravity * Time.deltaTime;
+            }
+
+            vertRotation -= mouseY * sensitivity;
+            vertRotation = Mathf.Max(-90, Mathf.Min(90, vertRotation));
+        
+            transform.Rotate(Vector3.up, mouseX * sensitivity);
+            cameraTransform.localRotation = Quaternion.Euler(vertRotation, 0, 0);
+
+            Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            Vector3 move = transform.transform.rotation * movement.normalized * moveSpeed;
+            if (Input.GetKey("space") && onGround)
+            {
+                yVelocity = jumpVelocity;
+            }
+
+            Vector3 velocity = (move + new Vector3(0, yVelocity, 0)) * Time.deltaTime;
+        
+            controller.Move(velocity);
+        
             yVelocity -= 0.5f * gravity * Time.deltaTime;
-        }
-
-        vertRotation -= mouseY * sensitivity;
-        vertRotation = Mathf.Max(-90, Mathf.Min(90, vertRotation));
         
-        transform.Rotate(Vector3.up, mouseX * sensitivity);
-        cameraTransform.localRotation = Quaternion.Euler(vertRotation, 0, 0);
+            if (Input.GetKeyDown("left ctrl"))
+            {
+                crouching = true;
+                tryingToStand = false;
+            }
+            if (Input.GetKeyUp("left ctrl"))
+            {
+               tryingToStand = true;
+            }
 
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Vector3 move = transform.transform.rotation * movement.normalized * moveSpeed;
-        if (Input.GetKey("space") && onGround)
-        {
-            yVelocity = jumpVelocity;
-        }
-
-        Vector3 velocity = (move + new Vector3(0, yVelocity, 0)) * Time.deltaTime;
-        
-        controller.Move(velocity);
-        
-        yVelocity -= 0.5f * gravity * Time.deltaTime;
-        
-        if (Input.GetKeyDown("left ctrl"))
-        {
-            crouching = true;
-            tryingToStand = false;
-        }
-        if (Input.GetKeyUp("left ctrl"))
-        {
-            tryingToStand = true;
-        }
-
-        if (crouching)
-        {
+            if (crouching)
+            {
             
-            this.transform.localScale = new Vector3(this.transform.localScale.x, Mathf.Lerp(this.transform.localScale.y, crouchScale, 0.1f), this.transform.localScale.z);
-            moveSpeed = ogMoveSpeed / 3;
+                this.transform.localScale = new Vector3(this.transform.localScale.x, Mathf.Lerp(this.transform.localScale.y, crouchScale, 0.1f), this.transform.localScale.z);
+                moveSpeed = ogMoveSpeed / 3;
 
-        }
-        else
-        {
-            this.transform.localScale = new Vector3(this.transform.localScale.x, Mathf.Lerp(this.transform.localScale.y, originalScale, 0.05f), this.transform.localScale.z);
-            moveSpeed = ogMoveSpeed;
-        }
+            }
+            else
+            {
+                this.transform.localScale = new Vector3(this.transform.localScale.x, Mathf.Lerp(this.transform.localScale.y, originalScale, 0.05f), this.transform.localScale.z);
+                moveSpeed = ogMoveSpeed;
+            }
 
-        if (crouching && tryingToStand && !Physics.Raycast(this.transform.position, Vector3.up, originalScale - this.transform.localScale.y))
-        {
-            tryingToStand = false;
-            crouching = false;
+            if (crouching && tryingToStand && !Physics.Raycast(this.transform.position, Vector3.up, originalScale - this.transform.localScale.y))
+            {
+                tryingToStand = false;
+                crouching = false;
+            }
         }
+    }
+
+    public void setIsPlaying()
+    {
+        isPlaying = !isPlaying;
     }
 }
